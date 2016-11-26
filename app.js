c63fc9ad1de3a609d00a20bc01b7d660be45becf
smallwolf <wolf@smallwolf.me>
2016-11-26 14:04:27 +0800

    Fix error

--- app.js
+++ app.js
@@ -37,11 +37,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = err
-  console.error(err.stack);
+  res.locals.error = err;
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  delete err.stack;
+  res.render('error', err);
 });
 
 module.exports = app;
