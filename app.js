339a47be455de64bcabc0efff40f7a66ce096127
smallwolf <wolf@smallwolf.me>
2016-11-26 14:14:17 +0800

    简单写个错误提示吧

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
 var app = express();
-if(!fs.existsSync(__dirname + '/./private_temp')){
+if (!fs.existsSync(__dirname + '/./private_temp')) {
   fs.mkdirSync(__dirname + '/./private_temp');
 }
 // view engine setup
@@ -20,28 +20,28 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = err;
-  // render the error page
-  res.status(err.status || 500);
+  res.status(err.status > 100 && err.status || 500);
   delete err.stack;
-  res.render('error', err);
+  if(err.status === 1){
+    err.message = "MAC地址不匹配，操作失败！";
+  }
+  res.render('error', {message: err.message});
 });
 
 module.exports = app;
